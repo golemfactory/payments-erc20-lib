7322d96669ed7660c89d9830fac38f0225f9da08
scx1332 <sieciech.czajka@golem.network>
2024-09-29 18:16:20 +0200

    Add groups to server

--- crates/web3_account_server/src/main.rs
+++ crates/web3_account_server/src/main.rs
@@ -5,7 +5,8 @@ use actix_web_httpauth::middleware::HttpAuthentication;
 use std::env;
 use std::fs::OpenOptions;
 use std::io::{BufReader, BufWriter};
-use std::sync::{Arc};
+use std::path::{Component, Path, PathBuf};
+use std::sync::Arc;
 use structopt::StructOpt;
 
 fn read_results(file_name: &str) -> Vec<String> {
@@ -119,6 +120,70 @@ async fn get_from_queue(data: web::Data<AppState>) -> impl Responder {
     }
 }
 
+fn get_file_name_from_filename_and_group(file_name: &str, group: &str) -> String {
+    let sanitized_group: String = group
+        .chars()
+        .filter(|c| c.is_alphanumeric() || *c == '_') // Allow only alphanumeric and underscores
+        .collect();
+
+    let sanitized_file_name = Path::new(file_name)
+        .components()
+        .filter(|comp| matches!(comp, Component::Normal(_))) // Allow only valid file names
+        .collect::<PathBuf>();
+
+    format!("{}_{}", sanitized_group, sanitized_file_name.display())
+}
+
+async fn add_to_queue_group(
+    data: web::Data<AppState>,
+    path: web::Path<String>,
+    item: String,
+) -> impl Responder {
+    let _lock = data.lock.lock().await;
+    let group = path.into_inner();
+    let file_name = get_file_name_from_filename_and_group(&data.file_name, &group);
+    let Ok(private_key) = hex::decode(item.replace("0x", "")) else {
+        return HttpResponse::BadRequest().body("Invalid item type");
+    };
+    if private_key.len() != 32 {
+        return HttpResponse::BadRequest().body("Invalid item length");
+    }
+    match add(hex::encode(private_key), &file_name) {
+        Ok(true) => HttpResponse::Ok().body("Added to the queue"),
+        Ok(false) => HttpResponse::Ok().body("Item already in the queue"),
+        Err(e) => {
+            log::error!("Error adding item: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+async fn count_group(data: web::Data<AppState>, path: web::Path<String>) -> impl Responder {
+    let _lock = data.lock.lock().await;
+    let group = path.into_inner();
+    let file_name = get_file_name_from_filename_and_group(&data.file_name, &group);
+    let results = read_results(&file_name);
+    HttpResponse::Ok().body(results.len().to_string())
+}
+
+async fn get_from_queue_group(
+    data: web::Data<AppState>,
+    path: web::Path<String>,
+) -> impl Responder {
+    let _lock = data.lock.lock().await;
+
+    let group = path.into_inner();
+    let file_name = get_file_name_from_filename_and_group(&data.file_name, &group);
+    match get(&file_name) {
+        Ok(Some(item)) => HttpResponse::Ok().body(item),
+        Ok(None) => HttpResponse::BadRequest().body("Queue is empty"),
+        Err(e) => {
+            log::error!("Error getting item: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
 fn get_env_access_token() -> String {
     env::var("BEARER_KEY").unwrap_or("change_me".to_string())
 }
@@ -167,6 +232,9 @@ async fn main() -> std::io::Result<()> {
             .route("/count", web::get().to(count))
             .route("/add", web::post().to(add_to_queue))
             .route("/get", web::get().to(get_from_queue))
+            .route("/count/{group}", web::get().to(count_group))
+            .route("/add/{group}", web::post().to(add_to_queue_group))
+            .route("/get/{group}", web::get().to(get_from_queue_group))
     })
     .bind(format!("{}:{}", args.http_addr, args.http_port))?
     .workers(1)
